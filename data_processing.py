132c22c64ee3dd06bb4ae63f3009cb9e6c5a6a27
sanyogitagupta <guptaaashu0513@gmail.com>
2023-09-12 14:57:14 +0530

    Added new function sum

---
+++ data_processing.py
@@ -0,0 +1,13 @@
+
+
+print('adding all data processing code related to fruad detection')
+
+
+a=5
+b=6
+
+def sum(a,b):
+    c=a+b
+    return c
+
+print('Adding a new function sum which is not correct')
